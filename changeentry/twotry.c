c8c0e72b0bfa85433b6e20dc2d73728d4f9a205f
cy_lxf@163.com <cy_lxf@163.com>
2020-07-25 22:56:09 +0800

    添加了注释文字。

--- changeentry/twotry.c
+++ changeentry/twotry.c
@@ -1,6 +1,17 @@
+/*
+示例如何修改程序的入口函数。正常编译程序入口函数是main，编译后执行
+程序输出“Function is secentry,specific entry.”，编译时修改入口函数
+为myentry，编译后，执行，程序输出"Function is main,default entry."
+如何修改请看HowToCompile.txt文件。
+
+Author：李晓峰
+Date：2020.7.25
+web：http://buuer_xxtxiaofeng.gitee.io/lxf/
+*/
+
 # include <stdio.h>
 
-// entry point function 
+// new entry point function 
 int myentry();  
   
 
