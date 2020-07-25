bbdd6df104da568b61cba216a96040bccbef8ba5
cy_lxf@163.com <cy_lxf@163.com>
2020-07-25 22:48:40 +0800

    上传changeentry，示例如何修改缺省的入口函数。

---
+++ changeentry/twotry.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+// entry point function 
+int myentry();  
+  
+
+int main()
+{
+	printf("Function is main,default entry.\n");
+	return 0;
+}
+
+int myentry()
+{
+	printf("Function is secentry,specific entry.\n");
+	return 0;
+}
