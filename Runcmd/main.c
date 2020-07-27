3cfcd52eed73bbef7fa4f8296b1bcc23f3ac4ae3
cy_lxf@163.com <cy_lxf@163.com>
2020-07-27 22:12:32 +0800

    添加了runcmd示例

---
+++ Runcmd/main.c
@@ -0,0 +1,16 @@
+/*
+Author:Li Xiaofeng
+Date:2020.7.27
+Description:
+示例如何在程序中运行指令。
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("在windows上打开记事本程序\n");
+    system("notepad.exe");
+    return 0;
+}
